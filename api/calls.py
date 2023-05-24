375e5300730276c034c48e098b3508c21d1c57a1
Juan Saldana <saldanaj97@gmail.com>
2023-05-24 15:05:03 -0500

    Token acquired successfully

--- api/calls.py
+++ api/calls.py
@@ -1,3 +1,34 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-print('Hello')
+# Load .env vars
+load_dotenv()
+
+# Set your client ID and client secret
+client_id = os.getenv("ClientID")
+client_secret = os.getenv("ClientSecret")
+
+
+def acquireToken():
+    # Set the API endpoint and parameters
+    endpoint = "https://accounts.spotify.com/api/token"
+    params = {
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
+    }
+
+    # Make the POST request
+    response = requests.post(endpoint, data=params)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Get the access token from the response
+        access_token = response.json()
+        return ("Access Token:", access_token)
+    else:
+        return ("Error:", response.text)
+
+
+print(acquireToken())
